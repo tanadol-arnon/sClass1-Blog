1c3a23fc732bfee06ab111ac5b2040680a44a7d5
arnon <arnon31047@gmail.com>
2020-04-08 15:08:19 +0700

    add blog-detail.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,23 @@ function initMap() {
           $('#navbar').css('padding', '25px')
       }
   })
+
+  $(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+  });
