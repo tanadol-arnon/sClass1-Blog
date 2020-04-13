0c3b4e05468368230086c302f9d887485ed90deb
arnon <arnon31047@gmail.com>
2020-04-13 10:46:08 +0700

    fixed recaptcha, to top, css

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,33 +6,17 @@ function initMap() {
         document.getElementById('map'), {zoom: 15, center: uluru});
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: uluru, map: map});
-  }
+}
 
-  $(window).scroll(function(){
-      var scrollTop = $(this).scrollTop();
-      if(scrollTop > 1){
-        $('nav').css('padding','5px 25px')
-      }else{
-          $('#navbar').css('padding', '25px')
-      }
-  })
+$(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if(scrollTop > 1){
+    $('nav').css('padding','5px 25px')
+    }else{
+        $('#navbar').css('padding', '25px')
+    }
+})
 
-  $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        nav: false,
-        dots: true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:2,
-            },
-            1000:{
-                items:3,
-            }
-        }
-    });
-  });
+$('.to-top').click(function (e){
+    $('html, body').animate({scrollTop: '0px'}, 800)
+})
