73ea43cddece45122297103e2f6e61f7691c9626
arnon <arnon31047@gmail.com>
2020-04-17 22:54:50 +0700

    show content blog-detail

--- blog-detail.php
+++ blog-detail.php
@@ -1,32 +1,46 @@
+<?php
+    require_once('php/connect.php');
+    
+    $sql = "SELECT * FROM articles WHERE id = '".$_GET['id']."' ";
+    $result = $conn->query($sql) or die($conn->error);
+
+    if($result->num_rows > 0){
+        $row = $result->fetch_assoc();
+    } else {
+        header('location: blog.php');
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog Detail</title>
+    <title><?php echo $row['subject'] ?></title>
     <!-- COMMON TAGS -->
     <meta charset="utf-8">
     <!-- Search Engine -->
-    <meta name="description" content="คาดหวังไว้ว่า จะสอนนักเรียนทุกคน ให้สามารถสร้างเว็บไซต์ขึ้นมาด้วยตัวเอง และเรียนรู้องค์ประกอบ ทุกอย่างที่จำเป็นต่อการเริ่มสร้างเว็บไซต์ขึ้นมา เพื่อให้สามารถประกอบอาชีพ, เข้าสมัครงาน, ทำโปรเจคจบ, หรือทำโปรเจคที่ตัวเองคาดหวังไว้ ให้สำเร็จ">
-    <meta name="image" content="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80">
-    <meta name="title" content="AppzStory Studio">
-    <meta name="keywords" content="html, javascript, js, css, php, mysql">
-    <meta name="robots" content="index, follow">
+    <meta property="description" content="<?php echo $row['sub_title'] ?>">
+    <meta property="image" content="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80">
+    <meta property="title" content="AppzStory Studio">
+    <meta property="keywords" content="html, javascript, js, css, php, mysql">
+    <meta property="robots" content="index, follow">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="language" content="English">
-    <meta name="author" content="HelloWorld">
+    <meta property="language" content="English">
+    <meta property="author" content="HelloWorld">
     <!-- Schema.org for Google -->
     <meta itemprop="name" content="AppzStory Studio">
-    <meta itemprop="description" content="คาดหวังไว้ว่า จะสอนนักเรียนทุกคน ให้สามารถสร้างเว็บไซต์ขึ้นมาด้วยตัวเอง และเรียนรู้องค์ประกอบ ทุกอย่างที่จำเป็นต่อการเริ่มสร้างเว็บไซต์ขึ้นมา เพื่อให้สามารถประกอบอาชีพ, เข้าสมัครงาน, ทำโปรเจคจบ, หรือทำโปรเจคที่ตัวเองคาดหวังไว้ ให้สำเร็จ">
+    <meta itemprop="description" content="<?php echo $row['sub_title'] ?>">
     <meta itemprop="image" content="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80">
     <!-- Open Graph general (Facebook, Pinterest & Google+) -->
-    <meta name="og:title" content="AppzStory Studio">
-    <meta name="og:description" content="คาดหวังไว้ว่า จะสอนนักเรียนทุกคน ให้สามารถสร้างเว็บไซต์ขึ้นมาด้วยตัวเอง และเรียนรู้องค์ประกอบ ทุกอย่างที่จำเป็นต่อการเริ่มสร้างเว็บไซต์ขึ้นมา เพื่อให้สามารถประกอบอาชีพ, เข้าสมัครงาน, ทำโปรเจคจบ, หรือทำโปรเจคที่ตัวเองคาดหวังไว้ ให้สำเร็จ">
-    <meta name="og:image" content="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80">
-    <meta name="og:url" content="https://blog-f1b94.web.app/">
-    <meta name="og:site_name" content="AppzStory Studio">
-    <meta name="og:type" content="website">
+    <meta property="og:title" content="AppzStory Studio">
+    <meta property="og:description" content="<?php echo $row['sub_title'] ?>">
+    <meta property="og:image" content="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80">
+    <meta property="og:url" content="https://blog-f1b94.web.app/">
+    <meta property="og:site_name" content="AppzStory Studio">
+    <meta property="og:type" content="website">
     <!-- Favicons -->
     <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicons/favicon-32x32.png">
@@ -34,9 +48,9 @@
     <link rel="manifest" href="assets/images/favicons/site.webmanifest">
     <link rel="mask-icon" href="assets/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
     <link rel="shortcut icon" href="assets/images/favicons/favicon.ico">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="msapplication-config" content="assets/images/favicons/browserconfig.xml">
-    <meta name="theme-color" content="#ffffff">
+    <meta property="msapplication-TileColor" content="#da532c">
+    <meta property="msapplication-config" content="assets/images/favicons/browserconfig.xml">
+    <meta property="theme-color" content="#ffffff">
     <!-- CSS link -->
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css">
@@ -50,10 +64,10 @@
     <?php include_once('includes/navbar.php') ?>
 
     <!-- Section Header -->  
-    <header class="jarallax" data-jarallax='{ "speed"; 0.6 }' style="background-image: url('https://images.unsplash.com/photo-1498569026542-39c64353e401?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80');">
+    <header class="jarallax" data-jarallax='{ "speed"; 0.6 }' style="background-image: url(<?php echo $row['image'] ?>);">
         <div class="page-image">
-            <h1 class="display-4 font-weight-bold">HTML คืออะไร</h1>
-            <p class="lead">ภาษาหลักที่ใช้ในการเขียนเว็บเพจ โดยใช้ tag ในการกำหนดการแสดงผล</p>
+            <h1 class="display-4 font-weight-bold"><?php echo $row['subject'] ?></h1>
+            <p class="lead"><?php echo $row['sub_title'] ?></p>
         </div>
     </header>
 
@@ -61,108 +75,14 @@
     <section class="container blog-content">
         <div class="row">
             <div class="col-12">
-                <h2><strong>HTML คืออะไร</strong></h2>
-                <p><strong>HTML5</strong>&nbsp;คือ ภาษามาร์กอัป ที่ใช้สำหรับเขียน website &nbsp;ซึ่ง&nbsp;HTML5&nbsp;นี้เป็นภาษาที่ถูกพัฒนาต่อมาจากภาษา HTML และพัฒนาขึ้นมาโดย WHATWG (The Web Hypertext Application Technology Working Group) โดยได้มีการปรับเพิ่ม Feature หลายๆอย่างเข้ามาเพื่อให้ผู้พัฒนาสามารถใช้งานได้ง่ายมากยิ่งขึ้น</p>
-                <h2>ข้อดีของ HTML5</h2>
-                <ol>
-                    <li>เว็บไซต์ที่สร้างจากภาษา&nbsp;<strong>HTML5&nbsp;</strong>สามารถแสดงผลได้กับทุก web browser&nbsp;</li>
-                    <li><strong>HTML5&nbsp;</strong>จะช่วยลดการใช้พวกปลั๊กอินพิเศษอย่างพวก Adobe Flash, Microsoft Silverlight, Apache Pivot สนับสนุน วิดีโอ และ องค์ประกอบเสียง รวมทั้ง สื่อมัลติมีเดียต่างๆมากขึ้น โดยไม่ต้องใช้ Flash</li>
-                    <li>มีการจัดการข้อผิดพลาดที่ดีขึ้น</li>
-                    <li>สคริปต์ใหม่ ที่จะมาแทนที่สคริปต์เดิม (เขียนโค้ดสั้นลง)</li>
-                    <li><strong>HTML5&nbsp;</strong>มีความเป็นอิสระสูง (คล้ายๆ XML )</li>
-                    <li><strong>HTML5&nbsp;</strong>ทำงานควบคู่กับ CSS3 ได้ดี ช่วยให้สามารถเพิ่มลูกเล่นต่างๆบนเว็บไซต์ได้สวยงามมากยิ่งขึ้น (CSS คือส่วนแสดงผล ที่นักออกแบบสามารถกำหนดสีสัน ตำแหน่ง ลักษณะเวลานำเมาส์ไปแหย่แล้วมีกระต่ายโผล่ออกมาจากโพรง หรือจับก้อนวัตถุในหน้าเว็บฯ ให้ชิดซ้ายชิดขวา ส่วน CSS3 คือเวอร์ชั่นที่ 3 ของ CSS )</li>
-                </ol>
-                <h2>Features ใหม่ๆ ของ HTML5</h2>
-                <ol>
-                    <li>Semantic Markup : การเพิ่ม Element ที่ อ่านง่ายมากขึ้น และช่วยให้ เราทำ SEO ได้มีประสิทธิภาพมากยิ่งขึ้น</li>
-                    <li>Form Enhancements : เพิ่มความสามารถของ Form ต่างๆ ไม่ว่าจะเป็น Input type, Attribute หรือ แม้แต่ Element</li>
-                    <li>Audio / Video: รองรับการอ่านไฟล์เสียง และ วีดีโอ โดยไม่จำเป็นต้องใช้ Embed Code ของ Third Party</li>
-                    <li>Canvas : ใช้ในการวาดรูป โดยจำเป็นต้องใช้ Javascriptช่วย</li>
-                    <li>ContentEditable : สามารถแก้ไข Content ได้โดยตรงผ่านทางหน้าเว็บ</li>
-                    <li>Drag and Drop : ลากวางObject ได้ เพื่อเพิ่มการ ตอบสนองระหว่างระบบกับผู้ใช้</li>
-                    <li>Persistent Data Storage : มีการจัดการที่ดีขึ้น โดยเก็บข้อมูลลงบนเครื่องของผู้ใช้</li>
-                </ol>
+                <?php echo $row['detail'] ?>
             </div>
             <div class="col-12">
                 <hr>
-                <p class="text-right text-muted">8 เมษายน 2020</p>
+                <p class="text-right text-muted"><?php echo date_format(new DateTime($row['update_at']),"j F Y") ?></p>
             </div>
             <div class="col-12">
-                <div class="owl-carousel owl-theme">
-                    <section class="col-12 p-2">
-                        <div class="card h-100">
-                            <a href="#" class="warpper-card-img">
-                                <img class="card-img-top" src="assets/images/blog/img1.jpg" alt="Coding1">
-                            </a>
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, voluptas suscipit. Fuga corporis ab harum accusantium reiciendis quo optio, saepe eius ea quae explicabo? Eum aliquam ex asperiores alias iusto. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                            <div class="p-3">
-                                <a href="#" class="btn btn-primary btn-block">Read More</a>
-                            </div>
-                        </div>
-                    </section>
-                    <section class="col-12 p-2">
-                        <div class="card h-100">
-                            <a href="#" class="warpper-card-img">
-                                <img class="card-img-top" src="assets/images/blog/img1.jpg" alt="Coding1">
-                            </a>
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, voluptas suscipit. Fuga corporis ab harum accusantium reiciendis quo optio, saepe eius ea quae explicabo? Eum aliquam ex asperiores alias iusto. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                            <div class="p-3">
-                                <a href="#" class="btn btn-primary btn-block">Read More</a>
-                            </div>
-                        </div>
-                    </section>
-                    <section class="col-12 p-2">
-                        <div class="card h-100">
-                            <a href="#" class="warpper-card-img">
-                                <img class="card-img-top" src="assets/images/blog/img1.jpg" alt="Coding1">
-                            </a>
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, voluptas suscipit. Fuga corporis ab harum accusantium reiciendis quo optio, saepe eius ea quae explicabo? Eum aliquam ex asperiores alias iusto. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                            <div class="p-3">
-                                <a href="#" class="btn btn-primary btn-block">Read More</a>
-                            </div>
-                        </div>
-                    </section>
-                    <section class="col-12 p-2">
-                        <div class="card h-100">
-                            <a href="#" class="warpper-card-img">
-                                <img class="card-img-top" src="assets/images/blog/img1.jpg" alt="Coding1">
-                            </a>
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, voluptas suscipit. Fuga corporis ab harum accusantium reiciendis quo optio, saepe eius ea quae explicabo? Eum aliquam ex asperiores alias iusto. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                            <div class="p-3">
-                                <a href="#" class="btn btn-primary btn-block">Read More</a>
-                            </div>
-                        </div>
-                    </section>
-                    <section class="col-12 p-2">
-                        <div class="card h-100">
-                            <a href="#" class="warpper-card-img">
-                                <img class="card-img-top" src="assets/images/blog/img1.jpg" alt="Coding1">
-                            </a>
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, voluptas suscipit. Fuga corporis ab harum accusantium reiciendis quo optio, saepe eius ea quae explicabo? Eum aliquam ex asperiores alias iusto. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                            <div class="p-3">
-                                <a href="#" class="btn btn-primary btn-block">Read More</a>
-                            </div>
-                        </div>
-                    </section>
-                  </div>
-            </div>
-            <div class="col-12">
-                <div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#helloworld" data-width="100%" data-numposts="5"></div>
+                <div class="fb-comments" data-width="100%" data-href="https://localhost/blog/blog-detail.php?id_helloworld=<?php $row['id']; ?>" data-numposts="5"></div>
                 <div id="fb-root"></div>
             </div>
         </div>
